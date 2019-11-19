4fd5dc26710f5edc988479955d2b146bf3d7116e
JHWebDeveloper <54760956+JHWebDeveloper@users.noreply.github.com>
2019-11-19 01:57:58 -0500

    Update index.js

--- src/main/index.js
+++ src/main/index.js
@@ -6,6 +6,8 @@ const dev = process.env.NODE_ENV === 'development'
 const mac = process.platform === 'darwin'
 let win = false
 
+const { app, BrowserWindow, Menu } = electron
+
 const openWindow = prefs => new BrowserWindow({
   ...prefs,
   show: false,
