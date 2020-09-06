18c958d7e6f432c92161097e7ea9642e8c35af08
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-09-06 13:25:37 -0400

    moved setContextMenu to separate module

--- src/main/preload/preload.js
+++ src/main/preload/preload.js
@@ -1,48 +1,7 @@
-import { remote } from 'electron'
+import * as contextMenu from './contextMenu'
 import sendMessage from './sendMessage'
 
-const interop = {}
-
-const textElements = 'input[type="text"], textarea'
-
-interop.setContextMenu = () => {
-  const menu = new remote.Menu()
-
-  if (process.env.NODE_ENV === 'development') {
-    let pos = {}
-    
-    menu.append(new remote.MenuItem({
-      label: 'Inspect Element',
-      click() {
-        remote.getCurrentWindow().inspectElement(pos.x, pos.y)
-      }
-    }))
-  
-    window.addEventListener('contextmenu', e => {
-      e.preventDefault()
-      pos = { x: e.x, y: e.y }
-      menu.popup(remote.getCurrentWindow())
-    })
-  } else {
-    const menuItems = [
-      new remote.MenuItem({ role: 'cut' }),
-      new remote.MenuItem({ role: 'copy' }),
-      new remote.MenuItem({ role: 'paste' }),
-      new remote.MenuItem({ type: 'separator' }),
-      new remote.MenuItem({ role: 'selectAll' })
-    ]
-  
-    menuItems.forEach(item => menu.append(item))
-  
-    window.addEventListener('contextmenu', e => {
-      e.preventDefault()
-  
-      if (e.target.matches(textElements) && !e.target.disabled) {
-        menu.popup(remote.getCurrentWindow())
-      }
-    })
-  }
-}
+const interop = Object.assign({}, contextMenu)
 
 if (process.env.NODE_ENV === 'development') {
 	window.REPLACE_WITH_NAMESPACE = Object.freeze({
