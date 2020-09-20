affbfc6ca222134aca92b7f3d59df20e5a954aaa
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-09-20 11:43:35 -0400

    consolidated prod and dev interop freezing when attaching to renderer

--- src/main/preload/preload.js
+++ src/main/preload/preload.js
@@ -3,10 +3,17 @@ import sendMessage from './sendMessage'
 
 const interop = Object.assign({}, contextMenu)
 
+
+// ---- ATTACH ALL TO RENDERER--------
+
+const ns = 'REPLACE_WITH_NAME_SPACE'
+
+const freeze = Object.freeze({
+	interop: Object.freeze(interop)
+})
+
 if (process.env.NODE_ENV === 'development') {
-	window.REPLACE_WITH_NAMESPACE = Object.freeze({
-		interop: Object.freeze(interop)
-	})
+	window[ns] = freeze
 } else {
-	contextBridge.exposeInMainWorld('REPLACE_WITH_NAMESPACE', { interop })
+	contextBridge.exposeInMainWorld(ns, freeze)
 }
