11d7f4170aae471ce1fa411821ffc8f1188543e2
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2020-10-27 14:34:21 -0400

    in function openWindow, set enableRemoteModule to false

--- src/main/index.js
+++ src/main/index.js
@@ -16,7 +16,7 @@ const openWindow = prefs => new BrowserWindow({
 		nodeIntegration: dev,
 		contextIsolation: !dev,
 		enableEval: false,
-		enableRemoteModule: true,
+		enableRemoteModule: false,
     preload: dev
       ? path.join(__dirname, 'preload', 'babelRegister.js')
       : path.join(__dirname, 'preload.js')
