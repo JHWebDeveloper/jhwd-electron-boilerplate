71f87683464c3df49b0f20a0011bf25b8b75457f
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2026-08-18 11:50:07 -0400

    feat: export shared constants from '../../shared/constants' for improved modularity

--- src/main/constants/index.ts
+++ src/main/constants/index.ts
@@ -1,5 +1,7 @@
 import path from 'node:path'
 
+export * from '../../shared/constants'
+
 export const IS_MAC = process.platform === 'darwin'
 
 export const IS_DEV = process.env.NODE_ENV === 'development'
