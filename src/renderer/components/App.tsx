9b323af72ea13a8341abde2a22ac98a9899c6fe8
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2026-08-18 14:48:29 -0400

    feat: wrap App component with ElectronAPIProvider for improved context management

--- src/renderer/components/App.tsx
+++ src/renderer/components/App.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 import '../css/global.css'
 
+import { ElectronAPIProvider } from '../context/ElectronAPI'
+
 export default function App() {
-	return <div><p>Hello World</p></div>
+	return (
+		<ElectronAPIProvider>
+			<div><p>Hello World</p></div>
+		</ElectronAPIProvider>
+	)
 }
