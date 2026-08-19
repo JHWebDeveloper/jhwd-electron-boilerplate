6dc9c425b7277b9fc95a1a82e2ec52a8d9b8e715
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2026-08-18 21:32:46 -0400

    refactor: reorganize IPC types for improved clarity and maintainability

--- src/shared/types/index.ts
+++ src/shared/types/index.ts
@@ -7,6 +7,18 @@ export interface IpcChannel {
 	}
 }
 
-export type PossiblePromise<T> = Promise<T> | T
+export type SafeKey<T, K extends PropertyKey> = K extends keyof T ? T[K] : undefined
 
-export type SafeResponse<T, K extends PropertyKey> = K extends keyof T ? T[K] : undefined
+export type IpcPayload<K extends keyof IpcChannel> = SafeKey<IpcChannel[K], 'payload'>
+
+export type IpcResponse<K extends keyof IpcChannel> = SafeKey<IpcChannel[K], 'response'>
+
+export type ChannelsWithPayload = {
+	[K in keyof IpcChannel]: IpcPayload<K> extends undefined ? never : K;
+}[keyof IpcChannel]
+
+export type ChannelsWithoutPayload = {
+	[K in keyof IpcChannel]: K extends ChannelsWithPayload ? never : K;
+}[keyof IpcChannel]
+
+export type PossiblePromise<T> = Promise<T> | T
