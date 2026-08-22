a146316334d83093c455a40332acff6cd6fbff44
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2026-08-22 00:47:10 -0400

    refactor: remove ElectronAPIProvider wrapper from App component

--- src/renderer/components/App.tsx
+++ src/renderer/components/App.tsx
@@ -2,14 +2,10 @@ import React from 'react'
 
 import '../css/global.css'
 
-import { ElectronAPIProvider } from '../context/ElectronAPI'
-
 export default function App() {
 	return (
-		<ElectronAPIProvider>
-			<div>
-				<p>Hello World</p>
-			</div>
-		</ElectronAPIProvider>
+		<div>
+			<p>Hello World</p>
+		</div>
 	)
 }
