b7659586308f20529cdfdd03599130ea55b459f2
Jonathan Hamilton <jonathanjhamilton@gmail.com>
2026-08-22 02:16:43 -0400

    refactor: remove unused ElectronAPIProvider component

--- src/renderer/context/ElectronAPI.tsx
+++ src/renderer/context/ElectronAPI.tsx
@@ -1,13 +1,7 @@
-import React, { createContext } from 'react'
+import { createContext } from 'react'
 
 import { NAMESPACE } from '../constants'
 
 const { electronAPI } = window[NAMESPACE]
 
 export const ElectronAPI = createContext(electronAPI)
-
-export function ElectronAPIProvider() {
-	return (
-		<ElectronAPI value={electronAPI} />
-	)
-}
